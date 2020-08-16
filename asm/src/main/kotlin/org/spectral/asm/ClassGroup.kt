753fd577ef93689a1b3e87e3b69586f3441ff3f8
kyle-escobar <kslrtips@gmail.com>
2020-08-15 23:23:37 -0400

    Cleanup

--- asm/src/main/kotlin/org/spectral/asm/ClassGroup.kt
+++ asm/src/main/kotlin/org/spectral/asm/ClassGroup.kt
@@ -1,10 +1,21 @@
 package org.spectral.asm
 
+import org.objectweb.asm.tree.ClassNode
+
 /**
  * Represents a group of classes from a class path.
  *
  * @constructor
  */
-class ClassGroup : MutableList<Class> by mutableListOf() {
+class ClassGroup() : MutableList<Class> by mutableListOf() {
 
+    /**
+     * Creates a class group with added [ClassNode] objects
+     *
+     * @param nodes The nodes to add
+     * @constructor
+     */
+    private constructor(nodes: Collection<ClassNode>) {
+
+    }
 }
